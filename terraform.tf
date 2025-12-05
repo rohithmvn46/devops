527135340f1dc31a331cd508036c38b429e91c98
rohithmvn46 <rohithmvn46@gmail.com>
2025-12-05 14:22:36 +0530

    terra script added

---
+++ terraform.tf
@@ -0,0 +1,7 @@
+resource "oci_database_db_system" "test_db" {
+  availability_domain = "AD-1"
+  compartment_id      = var.compartment_id
+  display_name        = "TestDB"
+  shape               = "VM.Standard2.4"
+  database_edition    = "ENTERPRISE_EDITION"
+}
